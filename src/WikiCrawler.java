faa6db858ee63a596aa0a004dfeda0c67c448cc7
Matthew Burket <m@tthewburket.com>
2018-04-19 10:56:25 -0500

    Started on Wiki Crawler

--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -12,16 +12,83 @@
  * @author Joel May
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class WikiCrawler {
     static final String BASE_URL = "https://en.wikipedia.org/";
+    private String seedUrl;
+    private int max;
+    private String fileName;
+    private ArrayList<String> topics;
+    private Queue<String> q = new LinkedList<>();
 
+    /**
+     * Create a Wiki Crawler
+     *
+     * @param seedUrl Page to start the crawl
+     * @param max number of pages
+     * @param topics list of topics
+     * @param fileName file to save output to
+     */
     public WikiCrawler(String seedUrl, int max, ArrayList<String> topics, String fileName) {
-        // implementation
+        this.seedUrl = seedUrl;
+        this.max = max;
+        this.fileName = fileName;
     }
 
-    public void crawl() {
-        // implementation
+    /**
+     * Crawls the BASE_URL based on seedUrl and BASE_URL
+     */
+    public void crawl() throws IOException {
+        q.add(seedUrl);
+        while (!q.isEmpty()) {
+            String currentPath = q.remove();
+            String html = getPageText(currentPath);
+        }
+    }
+
+    /**
+     * The the text of the given page
+     *
+     * @param path relative url
+     * @return text of the HTML page
+     */
+    private String getPageText(String path) throws IOException {
+        if (path == null || path.isEmpty() || path.trim().isEmpty()) {
+            throw new IllegalArgumentException("Must define a path");
+        }
+        URL theURL = new URL(BASE_URL + path);
+        InputStream inputStream = theURL.openStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Get the links from the page
+     *
+     * @param html html of the page as string
+     * @return a list of links
+     */
+    private ArrayList<String> getLinks(String html) {
+
+
     }
 }
