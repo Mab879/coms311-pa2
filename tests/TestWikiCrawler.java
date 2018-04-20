dc91c8dc47f35d8a8b54b4327e6237f620a8e092
Matthew Burket <m@tthewburket.com>
2018-04-20 00:31:36 -0500

    Added my test file

--- tests/TestWikiCrawler.java
+++ tests/TestWikiCrawler.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class TestWikiCrawler {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ArrayList<String> topics = new ArrayList<>();
+        topics.add("Iowa State");
+        topics.add("Cyclones");
+        WikiCrawler wc = new WikiCrawler("/wiki/Iowa_State_University", 100,  topics,"isu2");
+        wc.crawl();
+    }
 }
